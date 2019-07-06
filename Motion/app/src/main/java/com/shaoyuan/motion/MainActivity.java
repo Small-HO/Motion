40aa73a91f70c1970a8a8ead441963d015597d2a
WIN10\win 10 <shuanghaox@163.com>
2019-07-06 16:04:25 +0800

    增加App启动优化

--- Motion/app/src/main/java/com/shaoyuan/motion/MainActivity.java
+++ Motion/app/src/main/java/com/shaoyuan/motion/MainActivity.java
@@ -6,6 +6,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
+
     }
 }
